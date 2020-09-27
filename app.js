7a2f7c7e9271605f941a36cc53bc8a41f7ff9184
Julia Fong <Juliamzfong@gmail.com>
2020-09-27 14:10:18 -0500

    Add read me

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 console.log('Starting')
 
+setTimeout(() => {
+    console.log('2 second timers')
+}, 2000)
 
+
+setTimeout(() => {
+    console.log('right away timer')
+}, 0)
 console.log('Stopping')
