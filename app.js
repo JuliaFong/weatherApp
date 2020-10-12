9eb283dae107eeee6f929d6d18f75182c1583742
Julia Fong <Juliamzfong@gmail.com>
2020-10-12 13:27:31 -0500

    Refactor code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 
 // const url = 'http://api.weatherstack.com/current?access_key=6793779d8129c3035c99634b0bdec018&query=37.8267,-122.4233&units=f'
@@ -17,29 +18,12 @@ const request = require('request')
 
 //http://api.weatherstack.com/current?access_key=6793779d8129c3035c99634b0bdec018&query=37.8267,-122.4233
 
-// Geocoding
-//&limit=1
-// Address -> Lat/Long -> Weather
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVsaWFtZm9uZyIsImEiOiJjazN5dDBhbnMwNWl2M3NuMGM2aDV5cDhtIn0.VslPz2w-qm-yq_u4vyeOrA&limit=1'
-
-// request({ url: geocodeURL, json: true}, (error, response) => {
-//     if(error) {
-//         console.log("Unable to connect")
-//     } else if (response.body.features.length === 0) {
-//         console.log("Unable to find location")
-//     } else {
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(latitude, longitude)
-//     }
-
-// })
 
 
 
 
-geocode('Philadelphia New York', (error, data) => {
+
+geocode('Boston', (error, data) => {
     console.log('Error', error)
     console.log("Data", data)
 })
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,6 +1,5 @@
 const request = require('request')
 
-
 const geocode = (address, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoianVsaWFtZm9uZyIsImEiOiJjazN5dDBhbnMwNWl2M3NuMGM2aDV5cDhtIn0.VslPz2w-qm-yq_u4vyeOrA&limit=1"
 
@@ -21,3 +20,23 @@ const geocode = (address, callback) => {
 
 
 module.exports = geocode
+
+
+// Geocoding
+
+// Address -> Lat/Long -> Weather
+
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVsaWFtZm9uZyIsImEiOiJjazN5dDBhbnMwNWl2M3NuMGM2aDV5cDhtIn0.VslPz2w-qm-yq_u4vyeOrA&limit=1'
+
+// request({ url: geocodeURL, json: true}, (error, response) => {
+//     if(error) {
+//         console.log("Unable to connect")
+//     } else if (response.body.features.length === 0) {
+//         console.log("Unable to find location")
+//     } else {
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(latitude, longitude)
+//     }
+
+// })
