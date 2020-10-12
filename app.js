c5f3f85670767b136e62841af5e8a0c41797b66c
Julia Fong <Juliamzfong@gmail.com>
2020-10-11 21:52:26 -0500

    Add error handling to goecode logic

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const request = require('request')
 
 
-const url = 'http://api.weatherstack.com/current?access_key=6793779d8129c3035c99634b0bdec018&query=37.8267,-122.4233&units=f'
+// const url = 'http://api.weatherstack.com/current?access_key=6793779d8129c3035c99634b0bdec018&query=37.8267,-122.4233&units=f'
 
-request({ url: url, json: true }, (error, response) => {
-    // console.log(response.body.current)
-   
-    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelsLike + ' degrees out.')
-})
+// request({ url: url, json: true }, (error, response) => {
+//     // console.log(response.body.current)
+//   if(error) {
+//       console.log('Unable to connect to weather app')
+//   } else if (response.body.error) {
+//     console.log('Unable to find location')
+//   } else {
+//     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelsLike + ' degrees out.')
+    
+//   }
+// })
 
 //http://api.weatherstack.com/current?access_key=6793779d8129c3035c99634b0bdec018&query=37.8267,-122.4233
 
@@ -18,7 +24,14 @@ request({ url: url, json: true }, (error, response) => {
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVsaWFtZm9uZyIsImEiOiJjazN5dDBhbnMwNWl2M3NuMGM2aDV5cDhtIn0.VslPz2w-qm-yq_u4vyeOrA&limit=1'
 
 request({ url: geocodeURL, json: true}, (error, response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)
+    if(error) {
+        console.log("Unable to connect")
+    } else if (response.body.features.length === 0) {
+        console.log("Unable to find location")
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
+
 })
